5e6665c76bd64ef5ce32837a41bfcd37b9ae99ba
Melanie Roberts <robertsmelaniecc@gmail.com>
2025-08-11 10:49:41 -0400

    remove import cors

--- server/server.js
+++ server/server.js
@@ -3,7 +3,6 @@ import express from 'express';
 import multer from 'multer';
 import path from 'path';
 import { fileURLToPath } from 'url';
-import cors from 'cors';
 import OpenAI from 'openai';
 
 const app = express();
